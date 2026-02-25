479bc264b73ce716374815726d72c01736578bd8
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2026-02-25 09:23:32 -0500

    an extra comment

--- src/assembly.asm
+++ src/assembly.asm
@@ -9,6 +9,8 @@ EXTERN AssemblyPrint : PROC
 ; Creating an assembly method to be called by c++
 test_print PROC
     lea rcx, myString
+
+    ; I am not sure why i have to sub and add 28 but it crashes otherwise
     sub rsp, 28h
     call AssemblyPrint
     add rsp, 28h

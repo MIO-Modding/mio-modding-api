554323134dd14f7629dcb5d77116d30240ddb38f
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2026-06-29 02:24:48 -0400

    it works :D

--- MioModdingApi/MioModdingApi.cs
+++ MioModdingApi/MioModdingApi.cs
@@ -4,23 +4,8 @@ namespace MioModdingApi
 {
     public class MioModdingApi : Mod
     {
-        public MioModdingApi(Assembly assembly) : base(assembly)
+        public MioModdingApi(Assembly assembly, string name, string id, string[] dependencies) : base(assembly, name, id, dependencies)
         {
-        }
-
-        public override string[] GetDependencies()
-        {
-            return [];
-        }
-
-        public override string GetId()
-        {
-            return "io.github.mio-modding.mio-modding-api";
-        }
-
-        public override string GetName()
-        {
-            return "Mio Modding Api";
         }
 
         public override void Initialize()
--- TestMod/TestMod.cs
+++ TestMod/TestMod.cs
@@ -4,23 +4,8 @@ namespace TestMod
 {
     public class TestMod : Mod
     {
-        public TestMod(Assembly assembly) : base(assembly)
+        public TestMod(Assembly assembly, string name, string id, string[] dependencies) : base(assembly, name, id, dependencies)
         {
-        }
-
-        public override string[] GetDependencies()
-        {
-            return new string[] { "io.github.mio-modding.mio-modding-api" };
-        }
-
-        public override string GetId()
-        {
-            return "io.github.mio-modding.mio-modding-api.test-mod";
-        }
-
-        public override string GetName()
-        {
-            return "Test Mod";
         }
 
         public override void Initialize()

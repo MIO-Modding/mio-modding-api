9bd4a7c34132ad7affc43670fead0c1296fb7903
Cmdpro <73263645+Cmdpro@users.noreply.github.com>
2026-08-31 12:29:26 -0400

    Update TestMod.cs

--- TestMod/TestMod.cs
+++ TestMod/TestMod.cs
@@ -24,7 +24,7 @@ namespace TestMod
 
         private unsafe void Fixed_update_Prefix(MioGame.Game* __this)
         {
-            var mio = __this->mio;
+            ref var mio = ref __this->mio;
             if (mio.node != null && !mio.cutscene.active && !mio.walk_bot.active && mio.hook.state._value == MioGame.Mio.Hook.State.Inactive)
             {
                 mio.move_by_slide(new MioGame.Vec_float_3() { Base = new MioGame._vec_storage_float_3() { x = 0.1f } });
